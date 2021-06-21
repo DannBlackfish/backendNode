093ecd6b2d0ff315d4c9138c054c6b1f172b3dd6
DannBlackfish <monsterjunior@outlook.es>
2021-06-21 14:02:24 -0700

    headers

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(router);
 
 router.get('/message', function(req, res){
-    console.log(req.body);
-    console.log(req.query);
+    console.log(req.headers);
+    //specific hedears to our client:
+    res.header({
+        "custom-header": "Our personalized value"
+    })
     res.send('List of messages');
 })
 
 router.post('/message', function(req, res){
+    console.log(req.query)
+    console.log(req.body)
     res.send('Mensaje' + req.body.text + ' correctly added');
 })
 
