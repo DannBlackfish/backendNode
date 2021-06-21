afee79766fe43cbd646f1c035f6f0907ee52f9eb
DannBlackfish <monsterjunior@outlook.es>
2021-06-21 12:05:21 -0700

    proof get and post to insomnia

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res) {
-    res.send('Hola');
-});
+app.use(router);
+
+router.get('/message', function(req, res){
+    res.send('List of messages');
+})
+
+router.post('/message', function(req, res){
+    res.send('Add message');
+})
+
+// app.use('/', function (req, res) {
+//     res.send('Hola');
+// });
 
 app.listen(3000);
 
