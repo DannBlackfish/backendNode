164b8ff8113ea5302ff13b05340c2204f8f707a0
DannBlackfish <monsterjunior@outlook.es>
2021-06-21 13:46:58 -0700

    requestions query and body

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 var app = express();
-
+app.use(bodyParser.json);
 app.use(router);
 
 router.get('/message', function(req, res){
+    console.log(req.body);
+    console.log(req.query);
     res.send('List of messages');
 })
 
 router.post('/message', function(req, res){
-    res.send('Add message');
+    res.send('Mensaje' + req.body.text + ' correctly added');
 })
 
 // app.use('/', function (req, res) {
