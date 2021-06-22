2625eb34596de02922fe8685251fe0081e1fb839
DannBlackfish <monsterjunior@outlook.es>
2021-06-22 11:01:10 -0700

    mongodb, write and read

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,11 +1,26 @@
-const list = [];
+const db = require('mongoose');
+const Model = require('./model')
+
+const uri = 'mongodb://db_user_platzibackend:bLawWIFGJk69ZUHU@cluster0-shard-00-00.shvvz.mongodb.net:27017,cluster0-shard-00-01.shvvz.mongodb.net:27017,cluster0-shard-00-02.shvvz.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-h6uac6-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+db.Promise = global.Promise;
+
+db.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('[db] Correctly connected'))
+    .catch(err => console.error('[db]', err))
 
 function addMessage(message) {
-    list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-function getMessage() {
-    return list
+async function getMessage() {
+    //return list
+    const messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
