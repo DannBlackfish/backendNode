46fe61d505a16abe2c4fd67db04025f6909784b4
DannBlackfish <monsterjunior@outlook.es>
2021-06-22 19:00:59 -0700

    Connect APi to web socket

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
-
+const socket = require('./socket')
 const db = require ('./db')
-
 const router = require('./network/routes');
 
 db('mongodb://db_user_platzibackend:bLawWIFGJk69ZUHU@cluster0-shard-00-00.shvvz.mongodb.net:27017,cluster0-shard-00-01.shvvz.mongodb.net:27017,cluster0-shard-00-02.shvvz.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-h6uac6-shard-0&authSource=admin&retryWrites=true&w=majority');
 
-var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+socket.connect(server);
 router(app);
 
 app.use('/app', express.static('public'));
 
-app.listen(3000);
-console.log('The app is listening at http://localhost:3000')
+server.listen(3000, function () {
+    console.log('The app is listening at http://localhost:3000')
+});
